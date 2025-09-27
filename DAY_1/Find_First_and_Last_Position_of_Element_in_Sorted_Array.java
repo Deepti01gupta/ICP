8d7785e50cc13c70f47bc10af289da2940891680
Deepti Gupta <144911145+Deepti01gupta@users.noreply.github.com>
2025-09-27 13:21:28 +0530

    Update Find_First_and_Last_Position_of_Element_in_Sorted_Array.java

--- DAY_1/Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
+++ DAY_1/Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
@@ -2,4 +2,54 @@ package DAY_1;
 
 public class Find_First_and_Last_Position_of_Element_in_Sorted_Array {
 
+  //	https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+	public int[] searchRange(int[] nums, int target) {
+        int[] ans=new int[2];
+        int x1=first(nums,target);
+        int x2=last(nums,target);
+        ans[0]=x1;
+        ans[1]=x2;
+        return ans;
+    }
+
+    public int first(int[] arr, int t){
+        int lo=0;
+        int hi=arr.length-1;
+        int ans=-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(arr[mid]==t){
+                ans=mid;
+                hi=mid-1;
+            }
+            else if(arr[mid]>t){
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return ans;
+    }
+
+    public int last(int[] arr, int t){
+        int lo=0;
+        int hi=arr.length-1;
+        int ans=-1;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(arr[mid]==t){
+                ans=mid;
+                lo=mid+1;
+            }
+            else if(arr[mid]>t){
+                hi=mid-1;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return ans;
+    }
+    
 }
