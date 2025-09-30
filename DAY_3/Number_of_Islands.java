486c49a2eed66c3b4313acbbfb54a85ac998e062
Deepti Gupta <deepti20gupta05@gmail.com>
2025-09-30 11:26:04 +0530

    day-3

--- DAY_3/Number_of_Islands.java
+++ DAY_3/Number_of_Islands.java
@@ -5,7 +5,28 @@ public class Number_of_Islands {
 	
 //	https://leetcode.com/problems/number-of-islands/description/
 	public int numIslands(char[][] grid) {
-        return 0;
+        int ans=0;
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j]=='1'){
+                    check(grid,i,j);
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public void check(char[][] grid, int i, int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]=='0'){
+            return;
+        }
+        grid[i][j]='0';
+        check(grid,i+1,j);
+        check(grid,i-1,j);
+        check(grid,i,j+1);
+        check(grid,i,j-1);
+        
     }
 
 
