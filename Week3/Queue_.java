39412ae19d8756608ca30e124f3c8bd9054542ca
Deepti Gupta <deepti20gupta05@gmail.com>
2026-02-02 23:42:22 +0530

    Queue_ added

--- Week3/Queue_.java
+++ Week3/Queue_.java
@@ -1,10 +1,29 @@
 package Week3;
 
+import java.util.*;
+
 public class Queue_ {
+	
+//	https://codeforces.com/problemset/problem/353/D
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		
+		long count=0;
+		long time=0;
+		
+		for(char ch:s.toCharArray()) {
+			if(ch=='M') {
+				count++;
+			}
+			else if(count>0) {
+				time=Math.max(time+1, count);
+			}
+		}
+		System.out.println(time);
+		sc.close();
 	}
 
 }
--- Week4/Minimum_Pair_Removal_to_Sort_Array_II.java
+++ Week4/Minimum_Pair_Removal_to_Sort_Array_II.java
@@ -1,5 +1,8 @@
 package Week4;
 
 public class Minimum_Pair_Removal_to_Sort_Array_II {
-
+	
+//	https://leetcode.com/problems/minimum-pair-removal-to-sort-array-ii/
+	
+	
 }
